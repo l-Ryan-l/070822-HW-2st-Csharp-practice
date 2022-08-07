b0d7ed3fe26987f0b552e4a4ea8ab1c53d3d7797
Ryan <llryanll@yandex.ru>
2022-08-07 20:22:06 +0300

    task2 - Solved

--- task2-hideSecondDigit/Program.cs
+++ task2-hideSecondDigit/Program.cs
@@ -3,3 +3,10 @@
 // 782 -> 72
 // 918 -> 98
 
+int userNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Ваше число: {userNumber}");
+
+int digit1 = userNumber / 100;
+int digit3 = userNumber % 10;
+
+Console.Write($"Преобразованное число: {digit1}{digit3}");
