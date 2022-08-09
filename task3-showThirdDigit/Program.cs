9cb1b5523a886ff2b46354fd09853a635dff2f55
Ryan <llryanll@yandex.ru>
2022-08-10 00:16:15 +0300

    add task3

---
+++ task3-showThirdDigit/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+int userNumber = int.Parse(Console.ReadLine()!); 
+
+if((userNumber / 100 > 0) && (userNumber / 1000 == 0))
+{
+    int thirdDigit = userNumber % 10;
+    Console.WriteLine (thirdDigit);
+}
+else 
+{
+    while(userNumber.Lenght > 3) // тут неправильно! как сделать? 
+        userNumber = userNumber / 10;
+
+    Console.Write("Число не трехзначное! Попробуйте снова...");
+}
+
+// Как составить цикл с проверкой до трехзначного?
